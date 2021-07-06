f1c7645ffb2a92f7f36e5e339302671a92d95084
Manvendra Dattatrey <manvendradattatrey@gmail.com>
2021-07-06 14:57:33 +0530

    Adding basic styling to Home View

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -28,21 +28,27 @@ class HomeView extends React.Component {
             //current consistency score
             //today's targets
             <View style={styles.container}>
-                <TouchableOpacity>
-                    <Text>Menu</Text>
+                <TouchableOpacity style={styles.menuButton}>
+                    <Text style={styles.menuString}> Menu </Text>
                 </TouchableOpacity>
 
-                <Text>
-                    Current Consistency: {currConsistency}
-                </Text>
+                <View style={styles.score}>
+                    <Text style={styles.scoreString}>
+                        Current Consistency Score: {currConsistency}
+                    </Text>
+                </View>
 
-                <View style={styles.list}>
-                    <FlatList
-                    data={basicHabits}
-                    renderItem={({ item, index }) => (
-                        <Text key={index}> {item} </Text>
-                    )}
-                    keyExtractor={ ( item, index ) => `${index}` }
+                <View style={styles.ongoingTarget}>
+                    <Text style={styles.challengeHeading}>
+                        Today's Challenges:
+                    </Text>
+                    <FlatList 
+                        style={styles.ongoingTargetList}
+                        data={basicHabits}
+                        renderItem={({ item, index }) => (
+                            <Text style={styles.target} key={index}>- {item} </Text>
+                        )}
+                        keyExtractor={ ( item, index ) => `${index}` }
                     />
                 </View>
                 {/* <Text style={styles.appTitle}>HomeView</Text> */}
@@ -56,6 +62,50 @@ const styles = StyleSheet.create({
       flex:1,
     //   justifyContent: 'center',
     //   alignItems: 'center'
+    },
+    menuButton: {
+        alignSelf: 'baseline',
+        marginLeft: 10,
+        marginTop: 20,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 6,
+        padding: 5
+    },
+    menuString: {
+        fontSize: 18,
+        fontWeight: "900",
+        fontFamily: "sans-serif"
+    },
+    score: {
+        margin: 20,
+        alignItems: 'center'
+    },
+    scoreString: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    ongoingTarget: {
+        borderColor: '#bbb',
+        borderWidth: 4,
+        borderRadius: 4,
+        margin: 5,
+        padding: 5,
+        // flex:1
+        flexGrow:0,
+        flexShrink:1
+    },
+    challengeHeading: {
+        fontWeight:'bold', 
+        fontSize: 18,
+        margin: 4,
+        alignSelf: 'center'
+    },  
+    ongoingTargetList: {
+        // flexGrow: 2,
+    },
+    target: {
+        fontSize: 18
     },
     appTitle: {
       fontSize: 40,
